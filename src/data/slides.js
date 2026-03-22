0f74bce8da1498d225465ba8e81f2a25c00fa88d
bpollak <bpollak@ucsd.edu>
2026-03-21 19:33:30 -0700

    Revert "Further adjust Courtney headshot framing for closing slide size"
    This reverts commit 3471d3b8c49e6a78f7353c6e068d8bbb2aed89f3.

--- src/data/slides.js
+++ src/data/slides.js
@@ -2210,8 +2210,8 @@ export const slides = [
         "name": "Courtney Giordano",
         "title": "Associate Chancellor",
         "image": "/media/giordano.jpg",
-        "imageScale": 0.75,
-        "imagePosition": "center 25%"
+        "imageScale": 0.85,
+        "imagePosition": "center 30%"
       },
       {
         "name": "Brett Pollak",
